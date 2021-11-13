4144c006f4c756c39b5c0928d3ad35f53159b22c
Shawn Wildermuth <shawn@wildermuth.com>
2021-11-13 18:18:38 -0500

    Update Tests

--- src/bookcase/src/http/maps.spec.ts
+++ src/bookcase/src/http/maps.spec.ts
@@ -1,10 +1,11 @@
 import { Book } from "../models/Book";
 import { BookInfo } from "../models/bookInfo";
-import { mapBookInfoToBook } from "./maps";
+import { mapBookInfoToBook, mapDocToBook } from "./maps";
 import info from "../test/data/bookInfo";
+import doc from "../test/data/doc";
 
 describe("fonts", () => {
-  it("should provide fonts", () => {
+  it("should provide map Info to Book", () => {
 
     const book = mapBookInfoToBook(info);
 
@@ -13,5 +14,16 @@ describe("fonts", () => {
     expect(book.coverUrl === info.by_statement);
     expect(book.author === info.cover.medium);
 
+  });
+
+  it("should map Doc to Book", () => {
+    const book = mapDocToBook(doc);
+    expect(book);
+    if (book) {
+      expect(book.key === doc.key);
+      expect(book.title === doc.title);
+      expect(book.coverUrl);
+      expect(book.author);
+    }
   });
 });
---
+++ src/bookcase/src/test/data/doc.ts
@@ -0,0 +1,16 @@
+import { Doc } from "../../models/search"
+
+const doc: Doc =  {
+  "key": "/works/OL27448W",
+  "title": "The Lord of the Rings",
+  "edition_key": [
+      "OL33891821M",
+      "OL31973640M"
+  ],
+  "cover_edition_key": "OL21058613M",
+  "author_name": [
+      "J.R.R. Tolkien"
+  ]
+}
+
+export default doc;
